7e6cb4631237fe07cb55ab65c6e0e76376ec4503
wangliang <wangliang0585@163.com>
2014-10-20 15:57:01 +0800

    new project

---
+++ iWallPaper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iWallPaper
+//
+//  Created by wangliang-ms on 14/10/19.
+//  Copyright (c) 2014年 Noname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
